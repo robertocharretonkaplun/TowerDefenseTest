5b3d32a8d7f8eeee2c277b77d25419c5f086f811
Roberto Charreton <61713617+robertocharretonkaplun@users.noreply.github.com>
2022-02-05 17:45:32 -0600

    development - Turret Placing
    Added:
     - Basic turret placing
     - Node placing
     - Turret Manager

---
+++ TowerDefenseTest/Assets/TurretBuilder.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretBuilder : MonoBehaviour
+{
+  public static TurretBuilder instance;
+
+  private GameObject turret;
+  public GameObject basicTurretPref;
+
+  private void Awake()
+  {
+    if (instance != null)
+    {
+      return;
+    }
+      instance = this;
+  }
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    turret = basicTurretPref;
+  }
+
+  public GameObject GetTurret()
+  {
+    return turret;
+  }
+}
